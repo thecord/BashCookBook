5596cebe07be4ae80b5d115d21a8201c7766471e
Fahad Alyami <thecord2000@gmail.com>
2020-03-19 02:12:52 +0000

    Simple argument Bash Script

---
+++ arguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo " first argument :" $0
+echo " second argument :" $1
+echo " third argument :" $2
+echo " All arguments with \$@ :" $@
+echo " All arguments with \$* :" $*
+echo " All arguments Counts : $#"
