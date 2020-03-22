65b5eea2ecdc3c5862f440ea4b8ad25b034bf5f9
Fahad Alyami <thecord2000@gmail.com>
2020-03-22 02:43:08 +0000

    multiply with if statment

--- multiply.sh
+++ multiply.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/bash
 FIRST=$1
 SECOND=$2
+if [ ! $# -eq 2 ]; then
+	echo "Two arguments needed"
+	exit 1
+fi
 
+if [ $? -ne 0 ]; then
+	echo "Two arguments should be integer."
+	exit 2
+else
 let RESULT=FIRST*SECOND
 echo " $FIRST * $SECOND =  $RESULT"
+fi
 
-RESUTL1=$[ FIRST * SECOND ] 
-echo "$FIRST * $SECOND =  $RESUTL1"
+#RESUTL1=$[ FIRST * SECOND ] 
+#echo "$FIRST * $SECOND =  $RESUTL1"
+
